4517319756a89c763fe4d6bc56361c441e4efbad
iConceali <adrian.gc89@gmail.com>
2024-02-15 18:14:53 +0100

    update file forEach.js #206

--- staff/adrian-gordillo/arrays/forEach.js
+++ staff/adrian-gordillo/arrays/forEach.js
@@ -1,1 +1,22 @@
-Array.prototype.forEach;
+delete Array.prototype.forEach;
+
+function forEach(arr, mapForm) {
+  for (var i = 0; i < arr.length; i++) {
+    mapForm(arr[i]);
+  }
+}
+
+//CASE 1
+console.log("CASE 1");
+
+const array1 = [1, 4, 9, 16];
+
+// Pass a function to map
+const map1 = array1.map();
+
+console.log(map1);
+
+const arrayTest = [];
+const map2 = forEach(array1, (x) => console.log(x * 2));
+
+// Expected output: Array [2, 8, 18, 32]
