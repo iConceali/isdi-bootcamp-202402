0d8b6e3c4ce7e1e3d9d740489e79f65f2c6d252a
iConceali <adrian.gc89@gmail.com>
2024-02-21 10:39:54 +0100

    update splice.js and add reduce.js files #206

--- staff/adrian-gordillo/arrays/splice.js
+++ staff/adrian-gordillo/arrays/splice.js
@@ -2,6 +2,7 @@ delete Array.prototype.splice;
 
 function splice(array, index, numElement, element) {
   var newArr = [];
+  var extractArray = [];
 
   for (var i = 0; i < index; i++) {
     newArr[i] = array[i];
@@ -10,9 +11,11 @@ function splice(array, index, numElement, element) {
   if (numElement === 0) {
     newArr[index] = element;
   } else {
-    for (var i = index; i < index + numElement; i++) {
-      newArr[newArr.length] = element;
-    }
+    // for (var i = index; i < index + numElement; i++) {
+    newArr[newArr.length] = element;
+    //}
+    for (var i = 0; i < numElement; i++)
+      extractArray[extractArray.length] = array[index + i];
   }
 
   for (var i = index + numElement; i < array.length; i++) {
@@ -23,20 +26,45 @@ function splice(array, index, numElement, element) {
     array[i] = newArr[i];
   }
 
-  return newArr;
+  //array.length = index + numElement - 1;
+
+  return extractArray;
 }
 
 console.log("CASE 1");
 
 const months = ["Jan", "March", "April", "June"];
-splice(months, 1, 0, "Feb");
+var extract1 = splice(months, 1, 0, "Feb");
+
+console.log(extract1);
 // Inserts at index 1
 console.log(months);
 // Expected output: Array ["Jan", "Feb", "March", "April", "June"]
 
 console.log("CASE 2");
 
-splice(months, 4, 1, "May");
+var extract2 = splice(months, 4, 1, "May");
 // Replaces 1 element at index 4
 console.log(months);
 // Expected output: Array ["Jan", "Feb", "March", "April", "May"]
+console.log(extract2);
+
+console.log("CASE 3");
+
+const months1 = [
+  "Jan",
+  "Feb",
+  "March",
+  "April",
+  "May",
+  "June",
+  "July",
+  "August",
+  "September",
+];
+
+var extract3 = splice(months1, 3, 4, "X");
+// Replaces 1 element at index 4
+console.log(months1);
+// Expected output: Array ["Jan", "Feb", "March", "April", "May"]
+console.log(extract3);
